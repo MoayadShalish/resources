3570e7b1e6ad0f2513b9999345bf8f9ce5cd564d
Moayad Shalish <moayadshalish@hotmail.com>
2020-12-27 21:28:04 +0300

    solved the first question with reactive code

--- week-2-day-1/odevler/odev-3/app.js
+++ week-2-day-1/odevler/odev-3/app.js
@@ -1,6 +1,18 @@
-new Vue({
-        el: '#exercise',
-        data: {
-            value: 0
+const app = Vue.createApp({
+    data() {
+        return{
+        value: 0,
+        result: "",    
         }
-    });
+    },
+    methods: {
+        btn5() {
+            return this.value+=5;
+        },
+        btn1() {
+            return this.value+=1;
+        },
+        
+
+    }
+});
